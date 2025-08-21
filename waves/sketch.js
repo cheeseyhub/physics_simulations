b0f470aa90d21b5e157b9210f65038c032d1054a
cheeseyhub <muhammedibrahim613@gmail.com>
2025-08-21 21:09:30 +0500

    Sine Wave

--- waves/sketch.js
+++ waves/sketch.js
@@ -1,21 +1,32 @@
 let angle = 0;
-let angleV = 0.0002;
-let numPoints = 100;
+let angleV = 0.1;
+let numPoints = 50;
 let pointWidth = 0;
+let angles = [];
+let points = [];
+
 function setup() {
   createCanvas(windowWidth, windowHeight);
   pointWidth = width / numPoints;
+  for (let i = 0; i < numPoints; i++) {
+    angles[i] = i;
+  }
 }
 
 function draw() {
   background(0);
   stroke(255);
-  strokeWeight(3);
+  strokeWeight(pointWidth);
   for (let i = 0; i < numPoints; i++) {
-    let y = map(sin(angle), -1, 1, height / 2 - 100, height / 2 + 100);
+    let y = map(
+      sin(angles[i] * 0.2),
+      -1,
+      1,
+      height / 2 - 100,
+      height / 2 + 100
+    );
     let x = pointWidth * i;
-    point(pointWidth * i, y);
-    angle += angleV;
+    point(x, y);
+    angles[i] += angleV;
   }
- }
-
+}
